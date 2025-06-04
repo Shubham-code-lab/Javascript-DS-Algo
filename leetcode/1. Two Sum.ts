32a92f8f949ebf517efb863147f7dc4e640bfd0e
Shubham Shinde <74487161+Shubham-code-lab@users.noreply.github.com>
2025-06-04 15:02:34 +0530

    Create 1. Two Sum.ts

---
+++ leetcode/1. Two Sum.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+    const lookUpMap:any = new Map();
+     
+    for(let i = 0;i <= nums.length;i++){
+        const missingAdder =  target - nums[i];
+        if(lookUpMap.has(nums[i]))
+            return [lookUpMap.get(nums[i]), i];
+        else lookUpMap.set(missingAdder, i);
+    }
+};
