45a8669ff8918ce29ed052a4be3b9c51c8a0e671
Shubham Shinde <74487161+Shubham-code-lab@users.noreply.github.com>
2025-06-09 17:04:09 +0530

    Create 54. Spiral Matrix.ts

---
+++ leetcode/54. Spiral Matrix.ts
@@ -0,0 +1,21 @@
+function spiralOrder(matrix: number[][]): number[] {
+    let spiralList: number[] = [];
+    while (matrix.length > 0) {
+        spiralList.push(...matrix.shift());
+        for (let i = 0; i < matrix.length; i++) {
+            if (matrix[i].length > 0) {
+                spiralList.push(matrix[i].pop());
+            }
+        }
+        if (matrix.length > 0) {
+            spiralList.push(...(matrix.pop()!.reverse()));
+        }
+
+        for (let i = matrix.length - 1; i > -1; i--) {
+            if (matrix[i].length > 0) {
+                spiralList.push(matrix[i].shift());
+            }
+        }
+    }
+    return spiralList;
+};
